9bb3d3f23fbf33c424e6b71fca7cbdf6ea884c96
Sandeep Kumar <sandepten@gmail.com>
2024-03-12 11:12:15 +0530

    Refactor Navbar component to include a link to the dashboard

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,16 @@
 import { Menu, Triangle } from "lucide-react";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div className="border-primary/50 flex justify-between border px-5 py-3">
-      <div className="text-primary flex items-center gap-1 text-lg font-medium">
-        <Triangle className="size-6" />
-        <p>AltCoiners.live</p>
-      </div>
-      <Menu className="text-primary size-6" />
+    <div className="flex justify-between border border-primary/50 px-5 py-3">
+      <Link href="/dashboard">
+        <div className="flex items-center gap-1 text-lg font-medium text-primary">
+          <Triangle className="size-6" />
+          <p>AltCoiners.live</p>
+        </div>
+      </Link>
+      <Menu className="size-6 text-primary" />
     </div>
   );
 }
