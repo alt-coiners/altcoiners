0a0821e73f16ce8d0674604b3ea43a83b70a45c9
Sandeep Kumar <sandepten@gmail.com>
2024-03-16 16:32:28 +0530

    add: responsive navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,65 @@
-import { Menu, Triangle } from "lucide-react";
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+import { ChevronDown, Menu, Search, Triangle } from "lucide-react";
 import Link from "next/link";
+
+const MenuNavLinks = [
+  {
+    title: "News",
+    subMenus: [{ title: "New Window" }, { title: "Share" }, { title: "Print" }],
+  },
+  {
+    title: "Exclusives",
+    subMenus: [{ title: "New Window" }],
+  },
+  {
+    title: "Videos",
+    subMenus: [{ title: "New Window" }],
+  },
+  {
+    title: "Guides",
+    subMenus: [{ title: "New Window" }],
+  },
+  {
+    title: "Exchanges",
+    subMenus: [{ title: "New Window" }],
+  },
+];
 
 export default function Navbar() {
   return (
-    <div className="flex justify-between border border-primary/50 px-5 py-3">
+    <div className="flex items-center justify-between border border-primary/50 px-5 py-3">
       <Link href="/dashboard">
         <div className="flex items-center gap-1 text-lg font-medium text-primary">
           <Triangle className="size-6" />
           <p>AltCoiners.live</p>
         </div>
       </Link>
-      <Menu className="size-6 text-primary" />
+      <Menubar className="hidden border-0 lg:flex 2xl:gap-5">
+        {MenuNavLinks.map((menu, index) => (
+          <MenubarMenu key={index}>
+            <MenubarTrigger>
+              {menu.title} <ChevronDown className="size-4" />
+            </MenubarTrigger>
+            <MenubarContent>
+              {menu.subMenus.map((subMenu, index) => (
+                <>
+                  <MenubarItem key={index}>{subMenu.title}</MenubarItem>
+                  {index !== menu.subMenus.length - 1 && <MenubarSeparator />}
+                </>
+              ))}
+            </MenubarContent>
+          </MenubarMenu>
+        ))}
+      </Menubar>
+      <Menu className="size-6 text-primary lg:hidden" />
+      <Search className="size-6 text-primary" />
     </div>
   );
 }
