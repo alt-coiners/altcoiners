408dd15dd19993be3e42977bf247b317de5cfeae
Sandeep Kumar <sandepten@gmail.com>
2024-03-27 15:34:26 +0530

    add: navbar menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,26 +13,41 @@ const MenuNavLinks = [
   {
     title: "News",
     subMenus: [
-      { title: "Bitcoin Leaks", url: "/dashboard/news/" },
-      { title: "Share" },
-      { title: "Print" },
+      { title: "Bitcoin News", url: "/dashboard/news/Bitcoin" },
+      { title: "Ethereum News", url: "/dashboard/news/Ethereum" },
+      { title: "NFT News", url: "/dashboard/news/NFT" },
+      { title: "DeFi News", url: "/dashboard/news/DeFi" },
+      { title: "Altcoin News", url: "/dashboard/news/Altcoin" },
+      { title: "Blockchain News", url: "/dashboard/news/Blockchain" },
+      { title: "Finance News", url: "/dashboard/news/Finance" },
+      { title: "Technology News", url: "/dashboard/news/Technology" },
     ],
   },
   {
     title: "Exclusives",
-    subMenus: [{ title: "New Window" }],
+    subMenus: [{ title: "Opinions", url: "/dashboard/exclusives/Opinions" }],
   },
   {
     title: "Videos",
-    subMenus: [{ title: "New Window" }],
+    subMenus: [
+      { title: "Bitcoin Videos", url: "/dashboard/videos/Bitcoin" },
+      { title: "Ethereum Videos", url: "/dashboard/videos/Ethereum" },
+      { title: "NFT Videos", url: "/dashboard/videos/NFT" },
+      { title: "DeFi Videos", url: "/dashboard/videos/DeFi" },
+      { title: "Altcoin Videos", url: "/dashboard/videos/Altcoin" },
+      { title: "Blockchain Videos", url: "/dashboard/videos/Blockchain" },
+    ],
   },
   {
     title: "Guides",
-    subMenus: [{ title: "New Window" }],
+    subMenus: [
+      { title: "Bitcoin", url: "/dashboard/guides/1" },
+      { title: "Others", url: "/dashboard/guides/2" },
+    ],
   },
   {
     title: "Exchanges",
-    subMenus: [{ title: "New Window" }],
+    subMenus: [{ title: "Exchanges", url: "/dashboard/reviews" }],
   },
 ];
 
@@ -54,7 +69,9 @@ export default function Navbar() {
             <MenubarContent>
               {menu.subMenus.map((subMenu, index) => (
                 <>
-                  <MenubarItem key={index}>{subMenu.title}</MenubarItem>
+                  <Link href={subMenu.url} key={index}>
+                    <MenubarItem key={index}>{subMenu.title}</MenubarItem>
+                  </Link>
                   {index !== menu.subMenus.length - 1 && <MenubarSeparator />}
                 </>
               ))}
