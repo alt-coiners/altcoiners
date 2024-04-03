97cd947d0609bceec0b33b7ffd0c7137513af8f1
Sandeep Kumar <sandepten@gmail.com>
2024-04-03 12:54:18 +0530

    add: exchanges admin page

---
+++ src/app/admin/exchanges/_exchangesTable/columns.tsx
@@ -0,0 +1,77 @@
+import { Button } from "@/components/ui/button";
+import type { Exchange } from "@prisma/client";
+import { type ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown } from "lucide-react";
+import EditExchange from "./create";
+
+export const columns: ColumnDef<Exchange>[] = [
+  {
+    id: "edit",
+    header: ({ table }) => <Button variant="ghost">Edit</Button>,
+    cell: ({ row }) => <EditExchange id={row.original.id} />,
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "id",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          ID
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "name",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Name
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "url",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          URL
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "info",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Info
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      if (row.original.info.length > 200) {
+        return row.original.info.slice(0, 200) + "...";
+      }
+      return row.original.info;
+    },
+  },
+];
