689a5e26fe7d7622c5df92d10fa7b41ebadea1d2
Sandeep Kumar <sandepten@gmail.com>
2024-04-03 12:22:20 +0530

    Add Exchange model and Exchange router

---
+++ src/server/api/routers/exchange.ts
@@ -0,0 +1,46 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+
+export const exchangeRouter = createTRPCRouter({
+  getAllExchanges: publicProcedure.query(async ({ ctx }) => {
+    return await ctx.db.exchange.findMany();
+  }),
+
+  getExchangeById: publicProcedure
+    .input(z.object({ id: z.number() }))
+    .query(async ({ input, ctx }) => {
+      return await ctx.db.exchange.findUnique({
+        where: {
+          id: input.id,
+        },
+      });
+    }),
+
+  upsertExchange: publicProcedure
+    .input(
+      z.object({
+        id: z.number().optional(),
+        name: z.string(),
+        url: z.string(),
+        info: z.string(),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      return await ctx.db.exchange.upsert({
+        where: {
+          id: input.id,
+        },
+        update: {
+          name: input.name,
+          url: input.url,
+          info: input.info,
+        },
+        create: {
+          name: input.name,
+          url: input.url,
+          info: input.info,
+        },
+      });
+    }),
+});
