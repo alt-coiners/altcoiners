4b67bf38cd4f8080c82dc7d094289d31e8c33fd3
Sandeep Kumar <sandepten@gmail.com>
2024-04-29 11:44:55 +0530

    new icons for social media

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -69,22 +69,27 @@ export const FOOTER_LINKS = [
 export const SOCIAL_LINKS = [
   {
     title: "X",
-    icon: "/images/social/twitter.png",
+    icon: "/images/social/twitter.svg",
     href: "https://x.com/alt_coiners?s=21",
   },
   {
     title: "YouTube",
-    icon: "/images/social/youtube.png",
+    icon: "/images/social/youtube.svg",
     href: "https://youtube.com/@alt_coiners?si=b7vR-RAflFtH_fT3",
   },
   {
-    title: "Instagram",
-    icon: "/images/social/instagram.png",
+    title: "LinkedIn",
+    icon: "/images/social/linkedin.png",
     href: "https://youtube.com/@alt_coiners?si=b7vR-RAflFtH_fT3",
   },
   {
     title: "Telegram",
-    icon: "/images/social/telegram.png",
+    icon: "/images/social/telegram.svg",
     href: "https://t.me/AltCoinersCommunity",
+  },
+  {
+    title: "Discord",
+    icon: "/images/social/discord.svg",
+    href: "https://x.com/alt_coiners?s=21",
   },
 ];
