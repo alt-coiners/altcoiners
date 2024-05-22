022ad308c7a01f278fd58483e8aadcf8e32e0df6
Sandeep Kumar <sandepten@gmail.com>
2024-05-22 20:43:21 +0530

    Refactor AdSection component for better responsiveness

--- src/components/AdSection.tsx
+++ src/components/AdSection.tsx
@@ -27,7 +27,7 @@ export default function AdSection({ banner, className }: AdSectionProps) {
         width={300}
         height={250}
         quality={100}
-        className="w-full rounded-xl"
+        className="h-full w-full rounded-xl "
       />
     </Link>
   );
